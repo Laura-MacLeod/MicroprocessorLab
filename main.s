cc062fcb43f9aa081a5c059129f37a83f030354f
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-11-01 12:06:00 +0000

    Update main.s
    Made the number of loops per cycle in PORTD assigned by the status of PORTC LEDs.

--- main.s
+++ main.s
@@ -8,16 +8,22 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
+	movlw	0xff
+	movwf	TRISC, A	    ; Port D all inputs
+	
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movwf	TRISD, A	    ; Port C all outputs
+	
 	bra 	test
 loop:
-	movff 	0x06, PORTB
+    
+	movff 	0x06, PORTD
 	incf 	0x06, W, A
 test:
+    
 	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
+	movf	PORTC, W    ; Assigns value of port C to W
+	cpfsgt 	0x06, A	    ; Compares against W
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
